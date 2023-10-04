d34fa04d8e9cf7717d4d128b8466872bd954086d
Kirill Terekhin <kirill.terekhin.98@bk.ru>
2023-10-04 09:42:53 +0300

    Replacing api and adding styling

--- js/script.js
+++ js/script.js
@@ -2,28 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const products = new Products();
   const ui = new UI();
 
-  products.getData(url, options).then((food) => {
+  products.getData(url).then((food) => {
     ui.displayFood(food);
   });
 });
 
 // Request data
-const url = "https://the-mexican-food-db.p.rapidapi.com/";
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "b3dd7ebf57msh259bb2d3f3ded90p1365e4jsn0c176b92836d",
-    "X-RapidAPI-Host": "the-mexican-food-db.p.rapidapi.com",
-  },
-};
+const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
 
 // Getting the products
 class Products {
-  async getData(url, options) {
+  async getData(url) {
     try {
-      const response = await fetch(url, options);
+      const response = await fetch(url);
       const result = await response.json();
-      console.log(result);
+      console.log("Result-1", result);
       return result;
     } catch (error) {
       console.error(error);
@@ -36,20 +29,26 @@ class UI {
     const displayFood = document.getElementById("food-display");
     let amount = 6;
     let result = "";
-
+    console.log("Result-2", food);
+    console.log(food.categories[0]);
     for (let i = 0; i < amount; i++) {
       result += `
       <div class="meal">
           <div class="meal-img">
-            <img src="${food[i].image}" alt="" />
+            <img src="${food.categories[i].strCategoryThumb}" alt="" />
           </div>
           <div class="meal-txt">
-            <h3>${food[i].title}</h3>
-            <button type="button">Buy</button>
+            <h3>${food.categories[i].strCategory}</h3>
+            <button type="button" class='buy-btn'>Buy</button>
           </div>
         </div>
         `;
     }
     displayFood.innerHTML = result;
+  }
+
+  getFoodButtons() {
+    const buttons = [...document.querySelectorAll(".buy-btn")];
+    console.log(buttons);
   }
 }
